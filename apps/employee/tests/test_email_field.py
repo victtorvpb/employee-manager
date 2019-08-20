14552ba7df6dd4602129c45fbb9c8f005e3473bb
Victor Pereira <victtor.vpb@gmail.com>
2019-08-20 00:05:04 -0300

    fix test

--- apps/employee/tests/test_email_field.py
+++ apps/employee/tests/test_email_field.py
@@ -6,8 +6,8 @@ from apps.employee.utils.email_field import EmailField
 class TestsEmailField(TestCase):
     def test_email_field_lower_case(self):
         email = EmailField()
-        assert email.get_prep_value('Email@email.com') == 'email@email.com'
+        self.assertEqual(email.get_prep_value('Email@email.com'), 'email@email.com')
 
     def test_email_field_none(self):
         email = EmailField()
-        assert email.get_prep_value(None) is None
+        self.assertIsNone(email.get_prep_value(None))
